29da50d8aa9a7581630918dd5fadc9155ebdc83e
Timothy Lee <timothylhy@hotmail.com>
2025-09-28 02:23:12 +0800

    remove useless migrations

--- prisma/migrations/20250927171651_add_data_part/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterEnum
-ALTER TYPE "public"."message_part_type" ADD VALUE 'data_repositories';
-
--- AlterTable
-ALTER TABLE "public"."part" ADD COLUMN     "data_repositories_details" JSONB,
-ADD COLUMN     "data_repositories_id" TEXT;
